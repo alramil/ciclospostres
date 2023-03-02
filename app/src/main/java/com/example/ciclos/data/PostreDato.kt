87f6d4f753c7fc9616f41e685dc2ad3ac870ba25
alramil <alramil@unirioja.es>
2023-03-02 12:53:15 +0100

    final

---
+++ app/src/main/java/com/example/ciclos/data/PostreDato.kt
@@ -0,0 +1,22 @@
+package com.example.ciclos.data
+
+import com.example.ciclos.R
+import com.example.ciclos.model.Postre
+
+object PostreDato {
+    val listaPostre = listOf(
+        Postre(R.drawable.cupcake, 5, 0),
+        Postre(R.drawable.donut, 10, 5),
+        Postre(R.drawable.eclair, 15, 20),
+        Postre(R.drawable.froyo, 30, 50),
+        Postre(R.drawable.gingerbread, 50, 100),
+        Postre(R.drawable.honeycomb, 100, 200),
+        Postre(R.drawable.icecreamsandwich, 500, 500),
+        Postre(R.drawable.jellybean, 1000, 1000),
+        Postre(R.drawable.kitkat, 2000, 2000),
+        Postre(R.drawable.lollipop, 3000, 4000),
+        Postre(R.drawable.marshmallow, 4000, 8000),
+        Postre(R.drawable.nougat, 5000, 16000),
+        Postre(R.drawable.oreo, 6000, 20000)
+    )
+}
---
+++ app/src/main/java/com/example/ciclos/model/Postre.kt
@@ -0,0 +1,3 @@
+package com.example.ciclos.model
+
+data class Postre(val imageId: Int, val price: Int, val startProductionAmount: Int)
